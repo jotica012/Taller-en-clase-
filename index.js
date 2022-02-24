75c6211e69f4388d0e87e956a226c35abbfce575
jotica012 <88726635+jotica012@users.noreply.github.com>
2022-02-24 09:01:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let frase = "Look around look around, how lucky we are to be alive right now".toLocaleLowerCase();
+frase = frase.split("");
+
+
+console.log(frase);
+console.log(contadorRepetidas);
+
+
+
+function FirstLetterUnrepeated(frase) {
+  let contadorRepetidas = [];
+  let contadorNoRepetidas = [];
+  let primera = frase.indexOf(FirstLetterUnrepeated);
+
+  for (let i = 0; i < frase.length; i++) {
+    const letra = frase[i];
+
+    if (frase.indexOf(letra, i + 1)) {
+        contadorRepetidas.push();
+        
+    }
+  }
+}
